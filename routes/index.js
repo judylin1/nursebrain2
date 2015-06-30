c253a0a551075b2c6be573e0c8a3bdbe199d9747
Judy Lin <judith.s.lin@gmail.com>
2015-06-30 12:59:43 -0600

    dose calcs done

---
+++ routes/index.js
@@ -0,0 +1,120 @@
+var express = require('express');
+var router = express.Router();
+var unirest = require('unirest');
+var logic = require('./../public/javascripts/logic.js');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Nurse Brain 2' });
+});
+
+router.get('/calc', function(req, res, next) {
+  res.render('calculations/calc');
+});
+
+router.get('/dosecalc', function(req, res, next) {
+  res.render('calculations/dosecalc');
+});
+
+router.post('/dosecalc', function(req, res, next) {
+  var wtordered = req.body.wtordered;
+  var weight = req.body.weight;
+  var wtunit = req.body.wtunit;
+  if (wtunit === "kg") {
+  var final = wtordered * weight;
+  }
+  else {
+  var final = wtordered * weight * 0.453592;
+  }
+  res.render('calculations/dosecalc', {title: 'Nurse Brain 2',
+  wtordered: wtordered,
+  weight: weight,
+  final: Math.round(final) + ' mg'
+  });
+});
+
+router.post('/dosecalc2', function(req, res, next) {
+  var liqordered = req.body.liqordered;
+  var liqavailable = req.body.liqavailable;
+  var volume = req.body.volume;
+  if (liqordered.length <=0 || liqavailable.length <= 0 || volume.length <= 0) {
+    var liqfinal = "Check numbers."
+  }
+  else {
+    var liqfinal = Math.abs(((liqordered * volume)/liqavailable).toFixed(2)) + ' mL';
+  }
+  res.render('calculations/dosecalc', {title: 'Nurse Brain 2',
+  liqordered: liqordered,
+  liqavailable: liqavailable,
+  volume: volume,
+  liqfinal: liqfinal
+  });
+});
+
+router.post('/dosecalc3', function(req, res, next) {
+  var solidord = req.body.solidord;
+  var solidavail = req.body.solidavail;
+  if (solidord.length <= 0 || solidavail.length <= 0) {
+    var solidfinal = "Check numbers."
+  }
+  else {
+    var solidfinal = (solidord/solidavail).toFixed(2);
+  }
+  res.render('calculations/dosecalc', {title: 'Nurse Brain 2',
+  solidord: solidord,
+  solidavail: solidavail,
+  solidfinal: solidfinal
+});
+});
+
+router.get('/ivdrip', function(req, res, next) {
+  res.render('calculations/ivdrip');
+});
+
+router.post('/ivdrip', function(req, res, next) {
+  var ivtime = req.body.ivtime;
+  var ivvolume = req.body.ivvolume;
+  var hours = req.body.hours;
+  var minutes = req.body.minutes;
+  if (hours && ivtime > 0) {
+    var final = ivvolume/ivtime + ' hr/min';
+  }
+  else if (minutes && ivtime > 0) {
+    var final = ivvolume/(ivtime/60) + ' hr/min';
+  }
+  else {
+    var final = "Check numbers."
+  }
+  res.render('calculations/ivdrip', {title: 'Nurse Brain 2',
+  ivtime: ivtime,
+  ivvolume: ivvolume,
+  final: final
+});
+});
+
+router.post('/ivdrip2', function(req, res, next) {
+  var ivvol = req.body.ivvol;
+  var ivrate = req.body.ivrate;
+  if (ivvol.length <= 0 || ivrate.length <= 0) {
+    var ivans = "Check numbers."
+  }
+  else {
+    var ivans = (ivvol/ivrate).toFixed(2) + ' hours';
+  }
+  res.render('calculations/ivdrip', {title: 'Nurse Brain 2',
+  ivvol: ivvol,
+  ivrate: ivrate,
+  ivans: ivans
+});
+});
+
+router.get('/goodrx', function(req, res, next) {
+  res.render('goodrx/goodrx');
+});
+
+router.get('/drugsearch', function(req, res, next) {
+  res.render('drugsearch/drugsearch');
+});
+
+
+module.exports = router;
